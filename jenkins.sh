a06c97331e1b429679cbe195cef60863db31b5b3
sameer9644 <74947926+sameer9644@users.noreply.github.com>
2023-03-20 18:04:56 +0530

    jenkins.sh install

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo su
+yum install wget -y 
+hostnamectl set-hostname jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade  -y
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+cat /var/lib/jenkins/secrets/initialAdminPassword
+echo -e "set /etc/hosts file"
