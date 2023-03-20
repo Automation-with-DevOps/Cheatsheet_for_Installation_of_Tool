64328dcd14401a248ec9a9f049ceed0e42192027
sameer9644 <74947926+sameer9644@users.noreply.github.com>
2023-03-20 18:05:46 +0530

    docker.sh for docker

---
+++ docker.sh
@@ -0,0 +1,9 @@
+# install docker 
+
+#!/bin/bash
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl status docker
