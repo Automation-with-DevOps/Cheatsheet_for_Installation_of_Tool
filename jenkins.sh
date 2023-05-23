82e18b263f4212cb82168098b6fa3038e218b443
DevOpsien <74947926+DevOpsien@users.noreply.github.com>
2023-05-23 12:18:33 +0530

    docker installation file added in  jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo su
-yum install wget -y 
-hostnamectl set-hostname jenkins
+sudo yum update -y 
+sudo yum install wget -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl status docker
+sudo hostnamectl set-hostname jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
 sudo yum upgrade  -y
@@ -14,3 +19,4 @@ sudo systemctl start jenkins
 sudo systemctl status jenkins
 cat /var/lib/jenkins/secrets/initialAdminPassword
 echo -e "set /etc/hosts file"
+sudo usermod -a -G docker jenkins
