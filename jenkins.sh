46d66f152213fb7fec37afab86145bcfe599cec3
DevOpsien <74947926+DevOpsien@users.noreply.github.com>
2023-05-23 10:44:32 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ sudo su
 yum install wget -y 
 hostnamectl set-hostname jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
 sudo yum upgrade  -y
 # Add required dependencies for the jenkins package
 sudo yum install java-11-openjdk -y
