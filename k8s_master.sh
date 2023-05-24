a908a3e42cf5e5bad928215fb490000d075e3dcf
DevOpsien <74947926+DevOpsien@users.noreply.github.com>
2023-05-24 15:08:53 +0530

    sudo Update k8s_master.sh

--- k8s_master.sh
+++ k8s_master.sh
@@ -2,17 +2,18 @@
 
 #!/bin/bash
 #for master install 
-systemctl stop firewalld
-systemctl disable firewalld
-sed -i '/swap/d' /etc/fstab
-sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+sudo yum update -y 
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo sed -i '/swap/d' /etc/fstab
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
-yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
 
 
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+sudo cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
@@ -22,21 +23,21 @@ gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
 
-yum install yum-utils device-mapper-persistent-data lvm2 kubeadm docker-ce containerd.io -y
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2 kubeadm docker-ce containerd.io 
 
 sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 sudo systemctl enable --now kubelet
-rm -rf /etc/containerd/config.toml
+sudo rm -rf /etc/containerd/config.toml
 
-systemctl start docker
-systemctl enable docker
-systemctl status docker
-systemctl start containerd
-systemctl enable containerd
-systemctl status containerd
-systemctl enable kubelet
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo systemctl start containerd
+sudo systemctl enable containerd
+sudo systemctl status containerd
+sudo systemctl enable kubelet
 
-kubeadm init
+sudo kubeadm init
 
-echo -e "apply this command after kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml"
+sudo echo -e "apply this command after kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml"
